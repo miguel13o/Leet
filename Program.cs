f345df805659346c6445a9455a398dda0fb5e88b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-13 22:06:52 -0300

    Leet

--- Program.cs
+++ Program.cs
@@ -2,19 +2,19 @@
 
 Console.Write("texto a ser traduzido: ");
 Console.WriteLine(
-    Console.ReadLine()!
-        .Replace("A", "4")
-        .Replace("a", "4")
-        .Replace("E", "3")
-        .Replace("e", "3")
-        .Replace("I", "1")
-        .Replace("i", "1")
-        .Replace("L", "1")
-        .Replace("l", "1")
-        .Replace("O", "0")
-        .Replace("o", "0")
-        .Replace("T", "7")
-        .Replace("t", "7")
-        .Replace("S", "5")
-        .Replace("s", "5")
+Console.ReadLine()!
+.Replace("A", "4")
+.Replace("a", "4")
+.Replace("E", "3")
+.Replace("e", "3")
+.Replace("I", "1")
+.Replace("i", "1")
+.Replace("L", "1")
+.Replace("l", "1")
+.Replace("O", "0")
+.Replace("o", "0")
+.Replace("T", "7")
+.Replace("t", "7")
+.Replace("S", "5")
+.Replace("s", "5")
 );
